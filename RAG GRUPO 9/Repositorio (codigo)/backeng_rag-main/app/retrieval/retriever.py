2186c99117ed2fcca5fff78c2437ddd90eac02fa
Paulus2k <pcardonas@miumg.edu.gt>
2026-04-24 20:09:47 -0600

    Delete RAG GRUPO 9/Repositorio (codigo) directory

--- RAG GRUPO 9/Repositorio (codigo)/backeng_rag-main/app/retrieval/retriever.py
+++
@@ -1,8 +0,0 @@
-class Retriever:
-    def __init__(self, embedder, store):
-        self.embedder = embedder
-        self.store = store
-
-    def search(self, question: str, top_k: int = 4):
-        query_embedding = self.embedder.encode_query(question)
-        return self.store.query(query_embedding, top_k=top_k)
